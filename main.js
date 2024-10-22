acb1acdc173e30f336928bb4cef13cf3c4ee3176
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-10-21 23:47:33 -0300

    first web component

---
+++ main.js
@@ -0,0 +1,13 @@
+class Producto extends HTMLElement {
+    constructor(){
+        super();
+        const name = this.getAttribute("name");
+        const html = `<div class="card">
+        <img src="./img/${name}.jpg" alt="${name}">
+        <span>${name.toUpperCase}</span>
+    </div>`;
+    this.insertAdjacentHTML("beforeend", html);
+    }
+}
+
+customElements.define("producto", Producto);
